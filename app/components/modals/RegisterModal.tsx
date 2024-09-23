a79d92ae3ad4f34fa3d947206816503022dca57c
Ezequiel <ezequiel.h@lyncas.net>
2024-09-23 14:09:00 -0300

    Toast adition and footer to Register Modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -14,6 +14,8 @@ import useRegisterModal from "@/app/hooks/useRegisterModel";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
+import toast from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -41,7 +43,7 @@ const RegisterModal = () => {
                 registerModal.onClose();
             })
             .catch((error) => {
-                console.log(error);
+                toast.error("Something wnet wrong");
             })
             .finally(() => {
                 setIsLoading(false);
@@ -80,7 +82,42 @@ const RegisterModal = () => {
                 required
             />
         </div>
-    )
+    );
+
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button 
+                outline
+                label="Continue with Google"
+                icon={FcGoogle}
+                onCLick={() => {}}
+            />
+            <Button 
+                outline
+                label="Continue with Github"
+                icon={AiFillGithub}
+                onCLick={() => {}}
+            />
+            <div className="text-neutral-500 text-center mt-4 font-light">
+                <div className="flex justify-center flex-row items-center gap-2">
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div 
+                        onClick={registerModal.onClose}
+                        className="
+                            text-neutral-800
+                            cursor-pointer
+                            hover:underline
+                        "
+                    >
+                            Log in
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 
     return (
         <Modal 
@@ -91,6 +128,7 @@ const RegisterModal = () => {
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
+            footer={footerContent}
         />
     );
 }
